29373216b32144ce8f5ad1049319da779d3dcbd9
aidasyllapro-sys <aidasylla.pro@gmail.com>
2026-06-05 15:44:39 -0400

    Add download web page using reques and BeautifulSoup

---
+++ scripting_cyber/0x01-python_scripting/2-download_page.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def download_page(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+
+        soup = BeautifulSoup(response.text, "html.parser")
+        return soup.prettify()
+
+    except requests.exceptions.RequestException as e:
+        return str(e)
